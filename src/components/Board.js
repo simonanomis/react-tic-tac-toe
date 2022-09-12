22ca61cc78756b439b7f0473758c4c5c03ad0ada
Simona Ristovska <simona.ristovska@blackboard.com>
2022-09-12 14:14:55 +0200

    adding time travel: storing the history of moves and lifthing the state up to Game

--- src/components/Board.js
+++ src/components/Board.js
@@ -1,54 +1,20 @@
 import * as React from "react";
 import {Square} from "./Square";
 import '../index.css';
-import {calculateWinner} from "../calculate-winner";
 
 export class Board extends React.Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            squares: Array(9).fill(null),
-            //We’ll set the first move to be “X” by default.
-            xIsNext: true
-        }
-    }
-
-    handleClick(i) {
-        //we call .slice() to create a copy of the squares array to modify instead of modifying the existing array
-        const sq = this.state.squares.slice();
-        //return early by ignoring a click if someone has won the game or if a Square is already filled
-        if(calculateWinner(sq) || sq[i]) {
-            return;
-        }
-        sq[i] = this.state.xIsNext ? 'X' : 'O';
-        this.setState({
-            squares: sq,
-            xIsNext: !this.state.xIsNext
-        });
-    }
-
     renderSquare(i) {
         return (
             <Square
-                value={this.state.squares[i]}
-                onClick={() => this.handleClick(i)}
+                value={this.props.squares[i]}
+                onClick={() => this.props.onClick(i)}
             />
         );
     }
 
     render() {
-        const winner = calculateWinner(this.state.squares);
-        let status;
-
-        if(winner) {
-            status = 'Winner: ' + winner;
-        } else {
-            status = 'Next player: ' + (this.state.xIsNext ? 'X' : 'O')
-        }
-
         return (
             <div>
-                <div className="status">{status}</div>
                 <div className="board-row">
                     {this.renderSquare(0)}
                     {this.renderSquare(1)}
--- src/components/Game.js
+++ src/components/Game.js
@@ -1,16 +1,59 @@
 import {Board} from "./Board";
 import * as React from "react";
 import '../index.css';
+import {calculateWinner} from "../calculate-winner";
 
 export class Game extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            history: [{
+                squares: Array(9).fill(null),
+            }],
+            //We’ll set the first move to be “X” by default.
+            xIsNext: true
+        }
+    }
+
+    handleClick(i) {
+        const history = this.state.history;
+        const current = history[history.length - 1];
+        //we call .slice() to create a copy of the squares array to modify instead of modifying the existing array
+        const squares = current.squares.slice();
+        //return early by ignoring a click if someone has won the game or if a Square is already filled
+        if(calculateWinner(squares) || squares[i]) {
+            return;
+        }
+        squares[i] = this.state.xIsNext ? 'X' : 'O';
+        this.setState({
+            history: history.concat([{
+                squares: squares,
+            }]),
+            xIsNext: !this.state.xIsNext,
+        });
+    }
+
     render() {
+        const history = this.state.history;
+        const current = history[history.length - 1];
+        const winner = calculateWinner(current.squares);
+        let status;
+
+        if(winner) {
+            status = 'Winner: ' + winner;
+        } else {
+            status = 'Next player: ' + (this.state.xIsNext ? 'X' : 'O')
+        }
         return (
             <div className="game">
                 <div className="game-board">
-                    <Board />
+                    <Board
+                    squares={current.squares}
+                    onClick={(i) => this.handleClick(i)}
+                    />
                 </div>
                 <div className="game-info">
-                    <div>{/* status */}</div>
+                    <div>{status}</div>
                     <ol>{/* TODO */}</ol>
                 </div>
             </div>
